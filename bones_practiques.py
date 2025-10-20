6135677bc845efd4639a0f7e26b55f5d5a120524
ivanchavezreggio <ichavez@instituticaria.cat>
2025-10-20 08:07:44 +0200

    Bones practiques

--- bones_practiques.py
+++ bones_practiques.py
@@ -2,8 +2,12 @@
 '''Divisió. Programa que fá una divisio i torna els valors en un format concret.
 Institut Icària
 Programació - 1r Batxillerat - Curs 2025-26
-Donats dos nombres enters, el programa retorna la divisió, el quocient i el residu en un format molt especific.
+Donats dos nombres enters, el programa retorna la divisió, el quocient enter i el residu en un format molt especific.
 '''
+__author__ = "Iván Chavez Reggio"
+__email__ = "Ichavez@gmail.com"
+__date__ = "2025/10/20"
+
 Dividend = int(input("Ingresar dividend: "))
 Divisor = int(input("Ingresar divisor: "))
 Quocient = (Dividend) // (Divisor)
